2424e1f98ad82dfd6057c207f25cf09f7d4c1bb0
francisco-leal <francisco@talentprotocol.com>
2023-09-16 23:20:13 +0200

    Add vercel kv dependency

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
+// 1. Import modules.
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
+
+// 2. Set up your client with desired chain & transport.
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+})
+
 const server = Bun.serve({
   port: 3030,
-  fetch(req) {
-    return new Response("Bun!");
+  async fetch(req) {
+    const blockNumber = await client.getBlockNumber()
+    return new Response(`Bun! ${blockNumber}`);
   },
 });
 
